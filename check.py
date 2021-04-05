c65bbf4eaa21f3cab2dc4a2380e86d18bf007f3b
sidhanauk <sidhanauk@gmail.com>
2021-04-05 18:14:26 +0100

    test

--- check.py
+++ check.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import yaml
+import subprocess
 from tabulate import tabulate
 print("Inside python script")
 
@@ -11,14 +12,23 @@ headers = ["Environment","Component","Kind","Replicas", "Container Name", "CPU (
 
 
 for i in ["bld"]:
-    file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
+    dir_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i
 
-    yaml_file = open(file_path).read()
-    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader) 
-    spec = yaml_dict['spec']
-    container_spec=spec['template']['spec']['containers']
+    print(dir_path)
 
-    for x in container_spec:
+    out = subprocess.check_output("find ./${dir_path} -type f -exec grep -H 'Deployment\|StatefulSet' {} \;|awk -F: '{print $1}'", shell=True, universal_newlines=True)
+
+    file_list = out.split()
+
+    print(file_list)
+
+    for f in file_list:
+      yaml_file = open(f).read()
+      yaml_dict=yaml.load(yaml_file, yaml.SafeLoader) 
+      spec = yaml_dict['spec']
+      container_spec=spec['template']['spec']['containers']
+
+      for x in container_spec:
         obj_bld = []
         name = x['name']
         resources = x['resources']
@@ -36,58 +46,58 @@ for i in ["bld"]:
 
         table.append(obj_bld)
 
-for i in ["int"]:
-    file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
+# for i in ["int"]:
+#     file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
 
-    yaml_file = open(file_path).read()
-    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
-    spec = yaml_dict['spec']
-    container_spec=spec['template']['spec']['containers']
+#     yaml_file = open(file_path).read()
+#     yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
+#     spec = yaml_dict['spec']
+#     container_spec=spec['template']['spec']['containers']
 
-    for x in container_spec:
-        obj_int = []
-        name = x['name']
-        resources = x['resources']
+#     for x in container_spec:
+#         obj_int = []
+#         name = x['name']
+#         resources = x['resources']
        
-        obj_int.append(i.upper())
-        obj_int.append("Kafka")
-        obj_int.append(yaml_dict['kind'])    
-        obj_int.append(spec['replicas'])
-        obj_int.append(name)
+#         obj_int.append(i.upper())
+#         obj_int.append("Kafka")
+#         obj_int.append(yaml_dict['kind'])    
+#         obj_int.append(spec['replicas'])
+#         obj_int.append(name)
 
-        obj_int.append(resources['requests']['cpu'])
-        obj_int.append(resources['requests']['memory'])
-        obj_int.append(resources['limits']['cpu'])
-        obj_int.append(resources['limits']['memory'])
+#         obj_int.append(resources['requests']['cpu'])
+#         obj_int.append(resources['requests']['memory'])
+#         obj_int.append(resources['limits']['cpu'])
+#         obj_int.append(resources['limits']['memory'])
 
-        table.append(obj_int)
+#         table.append(obj_int)
 
-for i in ["prd"]:
-    file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
+# for i in ["prd"]:
+#     file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
 
-    yaml_file = open(file_path).read()
-    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
+#     yaml_file = open(file_path).read()
+#     yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
 
-    spec = yaml_dict['spec']
-    container_spec=spec['template']['spec']['containers']
+#     spec = yaml_dict['spec']
+#     container_spec=spec['template']['spec']['containers']
 
-    for x in container_spec:
-        obj_prd = []
-        name = x['name']
-        resources = x['resources']
+#     for x in container_spec:
+#         obj_prd = []
+#         name = x['name']
+#         resources = x['resources']
 
-        obj_prd.append(i.upper())
-        obj_prd.append("Kafka")
-        obj_prd.append(yaml_dict['kind'])    
-        obj_prd.append(spec['replicas'])
-        obj_prd.append(name)
+#         obj_prd.append(i.upper())
+#         obj_prd.append("Kafka")
+#         obj_prd.append(yaml_dict['kind'])    
+#         obj_prd.append(spec['replicas'])
+#         obj_prd.append(name)
 
-        obj_prd.append(resources['requests']['cpu'])
-        obj_prd.append(resources['requests']['memory'])
-        obj_prd.append(resources['limits']['cpu'])
-        obj_prd.append(resources['limits']['memory'])
+#         obj_prd.append(resources['requests']['cpu'])
+#         obj_prd.append(resources['requests']['memory'])
+#         obj_prd.append(resources['limits']['cpu'])
+#         obj_prd.append(resources['limits']['memory'])
 
-        table.append(obj_prd)
+#         table.append(obj_prd)
 
 
 print("************************************")
