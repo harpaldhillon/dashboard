e5a1d0e45c104a131f5c893fb4a35e4839b887fc
sidhanauk <sidhanauk@gmail.com>
2021-04-05 07:45:47 +0100

    test

--- check.py
+++ check.py
@@ -2,5 +2,19 @@
 
 import yaml
 from tabulate import tabulate
-
 print("Inside python script")
+
+charts = "kafka"
+
+headers = ["cpu","memory"]
+
+for i in ["bld","int","prd"]:
+    file_path="$WORKSPACE/out-dir-${i}/kafka/templates/deployment.yaml"
+
+
+    yaml_file = open(file_path).read()
+    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
+
+    spec = yaml_dict['spec']
+
+    print(spec)
