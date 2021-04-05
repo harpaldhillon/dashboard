344aeb6e0b1affc883160b45f1166f6fed3fd699
sidhanauk <sidhanauk@gmail.com>
2021-04-05 10:56:38 +0100

    test

--- check.py
+++ check.py
@@ -5,12 +5,7 @@ from tabulate import tabulate
 print("Inside python script")
 
 charts = "kafka"
-
-
 table = []
-obj_int = []
-#obj_bld = []
-obj_prd = []
 
 headers = ["Environment","Component","Kind","Replicas", "Container Name", "CPU (Request)", "Memory (Request)", "CPU (Limit)", "Memory (Limit)"]
 
@@ -19,14 +14,8 @@ for i in ["bld"]:
     file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
 
     yaml_file = open(file_path).read()
-    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
-
-    #obj_bld.append(i.upper())
-   # obj_bld.append("Kafka")
-    #obj_bld.append(yaml_dict['kind'])    
+    yaml_dict=yaml.load(yaml_file, yaml.SafeLoader) 
     spec = yaml_dict['spec']
-    #obj_bld.append(spec['replicas'])
-
     container_spec=spec['template']['spec']['containers']
 
     for x in container_spec:
@@ -38,8 +27,6 @@ for i in ["bld"]:
         obj_bld.append("Kafka")
         obj_bld.append(yaml_dict['kind'])    
         obj_bld.append(spec['replicas'])
-
-        
         obj_bld.append(name)
 
         obj_bld.append(resources['requests']['cpu'])
@@ -47,47 +34,27 @@ for i in ["bld"]:
         obj_bld.append(resources['limits']['cpu'])
         obj_bld.append(resources['limits']['memory'])
 
-        print("####################")
-        print(obj_bld)
-        print("####################")
-
         table.append(obj_bld)
-
-
 
 for i in ["int"]:
     file_path="/home/jenkins/agent/workspace/dashboard/out-dir-"+i+"/kafka/templates/deployment.yaml"
 
     yaml_file = open(file_path).read()
     yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
-
-    print("****kind******")
-    print(yaml_dict['kind'])
-
-    obj_int.append(i.upper())
-    obj_int.append("Kafka")
-    obj_int.append(yaml_dict['kind'])
-
-
     spec = yaml_dict['spec']
-
-    #print(spec)
-
-    print("****replicas******")
-    print(spec['replicas'])
-
-    obj_int.append(spec['replicas'])
-
     container_spec=spec['template']['spec']['containers']
 
     for x in container_spec:
+        obj_int = []
         name = x['name']
         resources = x['resources']
-        print("************************************")
-        print(i.upper(), "CPU", resources['requests']['cpu'])
-        print(i.upper(), "Memory", resources['requests']['memory'])
-
+       
+        obj_int.append(i.upper())
+        obj_int.append("Kafka")
+        obj_int.append(yaml_dict['kind'])    
+        obj_int.append(spec['replicas'])
         obj_int.append(name)
+
         obj_int.append(resources['requests']['cpu'])
         obj_int.append(resources['requests']['memory'])
         obj_int.append(resources['limits']['cpu'])
@@ -99,33 +66,20 @@ for i in ["prd"]:
     yaml_file = open(file_path).read()
     yaml_dict=yaml.load(yaml_file, yaml.SafeLoader)
 
-    print("****kind******")
-    print(yaml_dict['kind'])
-
-    obj_prd.append(i.upper())
-    obj_prd.append("Kafka")
-    obj_prd.append(yaml_dict['kind'])
-
-
     spec = yaml_dict['spec']
-
-    print(spec)
-
-    print("****replicas******")
-    print(spec['replicas'])
-
-    obj_prd.append(spec['replicas'])
-
     container_spec=spec['template']['spec']['containers']
 
     for x in container_spec:
+        obj_prd = []
         name = x['name']
         resources = x['resources']
-        print("************************************")
-        print(i.upper(), "CPU", resources['requests']['cpu'])
-        print(i.upper(), "Memory", resources['requests']['memory'])
 
+        obj_prd.append(i.upper())
+        obj_prd.append("Kafka")
+        obj_prd.append(yaml_dict['kind'])    
+        obj_prd.append(spec['replicas'])
         obj_prd.append(name)
+
         obj_prd.append(resources['requests']['cpu'])
         obj_prd.append(resources['requests']['memory'])
         obj_prd.append(resources['limits']['cpu'])
