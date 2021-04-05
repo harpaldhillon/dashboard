5ce9d02f85c2c1b80bda219bc11ec8fc7cbac714
sidhanauk <sidhanauk@gmail.com>
2021-04-05 07:53:08 +0100

    test

--- check.py
+++ check.py
@@ -16,4 +16,14 @@ for i in ["bld","int","prd"]:
 
     spec = yaml_dict['spec']
 
-    print(spec)
+    #print(spec)
+
+    container_spec=spec['template']['spec']['containers']
+
+    for x in container_spec:
+        resources = x['resources']
+        print("************************************")
+        print(i.upper(), "CPU", resources['requests']['cpu'])
+        print(i.upper(), "Memory", resources['requests']['memory'])
+
+        print(tabulate(resources['requests'], headers, tablefmt="grid"))
